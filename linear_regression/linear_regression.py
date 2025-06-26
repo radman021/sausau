df6c9ae13c0e658f3026e4e1486f9c69afdfcf6f
radman021 <ivannradman@gmail.com>
2025-06-26 20:12:28 +0200

    Added comments to linear regressions

--- linear_regression/linear_regression.py
+++ linear_regression/linear_regression.py
@@ -5,29 +5,36 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 
+# load existing dataset
 diabetes = datasets.load_diabetes()
 
+# select only one feature
 bmi = diabetes.data[:, 3]
 X = bmi.reshape(-1, 1)
 y = diabetes.target
 
+# split the dataset
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
+# create and fit model
 model = LinearRegression()
 model.fit(X_train, y_train)
+
+# make predictions for test data
 y_pred = model.predict(X_test)
 
+# calculate and show metrics
 mse = mean_squared_error(y_test, y_pred)
 mae = mean_absolute_error(y_test, y_pred)
 r2_score = r2_score(y_test, y_pred)
-
 
 print(f"Mean squared error (MSE): {mse}")
 print(f"Mean squared error (MAE): {mae}")
 print(f"Coefficient of determination {r2_score}")
 
+# plot results
 plt.scatter(X_test, y_test, color="black", label="Actual data")
 plt.plot(X_test, y_pred, color="blue", linewidth=2, label="Linear regression line")
 plt.xlabel("BMI")
--- linear_regression/lr_ridge_lasso_comparison.py
+++ linear_regression/lr_ridge_lasso_comparison.py
@@ -3,14 +3,17 @@ from sklearn.linear_model import Ridge, Lasso, LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 
+# load dataset (keeping all features!)
 diabetes = load_diabetes()
 X = diabetes.data
 y = diabetes.target
 
+# split dataset
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
+# create and fit LR, Ridge and Lasso regressors
 lr_model = LinearRegression()
 lr_model.fit(X_train, y_train)
 y_pred_lr = lr_model.predict(X_test)
@@ -22,6 +25,12 @@ y_pred_ridge = ridge_model.predict(X_test)
 lasso_model = Lasso(alpha=0.1)
 lasso_model.fit(X_train, y_train)
 y_pred_lasso = lasso_model.predict(X_test)
+
+# display and compare all results + discussion:
+# - what are the differences?
+# - what happened to mse?
+# - what happened to coefficients?
+# - what is conclusion?
 
 print("LR MSE:", mean_squared_error(y_test, y_pred_lr))
 print("LR Coefficients:", lr_model.coef_)
